21b67ebb72f0edc69b5a59c23302a076c35d6e60
wac <wababam9@gmail.com>
2023-07-07 04:44:49 -0700

    Fix linter errors

--- static/js/modal_menu.js
+++ static/js/modal_menu.js
@@ -1,11 +1,11 @@
 const burger = document.querySelector('.burger');
 const close = document.querySelector('.close');
-const modal_menu = document.querySelector('.modal_menu');
+const modalMenu = document.querySelector('.modal_menu');
 
 burger.addEventListener('click', () => {
-    modal_menu.show();
+  modalMenu.show();
 });
 
 close.addEventListener('click', () => {
-    modal_menu.close();
+  modalMenu.close();
 });
--- static/js/speakers.js
+++ static/js/speakers.js
@@ -1,82 +1,60 @@
 const parent = document.getElementById('featured');
-
+let flagExpanded = false;
 const data = [
-    {
-        img_path: 'static/images/miller-c.jpg',
-        name: 'Charlie Miller',
-        occupation: 'A world-class white hat hacker.',
-        description: 'Charlie won the Pwn2Own computer hacking contest four times. He found a vulnerability that would allow hacking into 1.4 million vehicles. He has worked for many high-tech companies including Twitter and Uber.'
-
-    },
-    {
-        img_path: 'static/images/greg.jpg',
-        name: 'Greg Hoglund',
-        occupation: 'A specialist in computer forensics',
-        description: 'Hoglund is a crucial feature in our list of ethical hackers. Some of the areas he is best known for working in include physical memory forensics, attribution of hackers and malware detection.'
-    },
-    {
-        img_path: 'static/images/kevin2.jpg',
-        name: 'Kevin Mitnick',
-        occupation: 'Former Black Hat hacker, the most high-profile hacker in recent history',
-        description: 'Mitnick’s activities breaching major corporations for his amusement earned him a place on the FBI’s most-wanted list. He was arrested in 1995 and served five years in prison.'
-    },
-    {
-        img_path: 'static/images/Tsutomu-Shimomura.jpg',
-        name: 'Tsutomu Shimomura',
-        occupation: 'A cybersecurity expert, physicist, and is credited with tracking down Kevin Mitnick.',
-        description: 'Being a computational physics research scientist, Shimomura also worked for the National Security Agency (NSA). He was known to be one of the leading researchers who raised awareness of the lacking security and privacy of cellular phones at that time.'
-    },
-    {
-        img_path: 'static/images/1200px-Dan_Kaminsky_cropped.jpg',
-        name: 'Dan Kaminsky',
-        occupation: 'One of the most famous white hat hackers.',
-        description: 'In 2008 he found a DNS flaw which allowed black hat hackers to redirect requests from one website to another in seconds. Thanks to Kaminsky’s discovery the vulnerability was fixed quickly after.'
-    },
-    {
-        img_path: 'static/images/British-scientist-Tim-Berners-Lee.webp',
-        name: 'Tim Berners-Lee',
-        occupation: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
-        description: 'One of the most famous names in computer science, Berners-Lee is the founder of the World Wide Web. Today he serves as the director of the World Wide Web Consortium (W3C), which oversees the development of the web.'
-    }
+  {
+    img_path: 'static/images/miller-c.jpg',
+    name: 'Charlie Miller',
+    occupation: 'A world-class white hat hacker.',
+    description:
+      'Charlie won the Pwn2Own computer hacking contest four times. He found a vulnerability that would allow hacking into 1.4 million vehicles. He has worked for many high-tech companies including Twitter and Uber.',
+  },
+  {
+    img_path: 'static/images/greg.jpg',
+    name: 'Greg Hoglund',
+    occupation: 'A specialist in computer forensics',
+    description:
+      'Hoglund is a crucial feature in our list of ethical hackers. Some of the areas he is best known for working in include physical memory forensics, attribution of hackers and malware detection.',
+  },
+  {
+    img_path: 'static/images/kevin2.jpg',
+    name: 'Kevin Mitnick',
+    occupation:
+      'Former Black Hat hacker, the most high-profile hacker in recent history',
+    description:
+      'Mitnick’s activities breaching major corporations for his amusement earned him a place on the FBI’s most-wanted list. He was arrested in 1995 and served five years in prison.',
+  },
+  {
+    img_path: 'static/images/Tsutomu-Shimomura.jpg',
+    name: 'Tsutomu Shimomura',
+    occupation:
+      'A cybersecurity expert, physicist, and is credited with tracking down Kevin Mitnick.',
+    description:
+      'Being a computational physics research scientist, Shimomura also worked for the National Security Agency (NSA). He was known to be one of the leading researchers who raised awareness of the lacking security and privacy of cellular phones at that time.',
+  },
+  {
+    img_path: 'static/images/1200px-Dan_Kaminsky_cropped.jpg',
+    name: 'Dan Kaminsky',
+    occupation: 'One of the most famous white hat hackers.',
+    description:
+      'In 2008 he found a DNS flaw which allowed black hat hackers to redirect requests from one website to another in seconds. Thanks to Kaminsky’s discovery the vulnerability was fixed quickly after.',
+  },
+  {
+    img_path: 'static/images/British-scientist-Tim-Berners-Lee.webp',
+    name: 'Tim Berners-Lee',
+    occupation: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
+    description:
+      'One of the most famous names in computer science, Berners-Lee is the founder of the World Wide Web. Today he serves as the director of the World Wide Web Consortium (W3C), which oversees the development of the web.',
+  },
 ];
 
-const content = [];
-
-window.addEventListener("resize", function () {
-    let width = window.matchMedia("(min-width: 768px)");
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        count = data.length;
-        populateFutureSpeakers(count);
-    }
-    else {
-        count = 2;
-        populateFutureSpeakers(count);
-        moreButtonListener();
-    }
-});
-
-
-window.addEventListener("load", function () {
-    let width = window.matchMedia("(min-width: 768px)");
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        count = data.length;
-        populateFutureSpeakers(count);
-    }
-    else {
-        count = 2;
-        populateFutureSpeakers(count);
-    }
-});
-
 function populateFutureSpeakers(count) {
-    const title = `<h2>Featured Speakers</h2>
+  const title = `<h2>Featured Speakers</h2>
     <hr class="line">`;
-    parent.innerHTML = title;
+  parent.innerHTML = title;
 
-    let wrapper = `<div class="wrapper">`;
-
-    for (let i = 0; i < count; i += 1) {
-        let speaker = `<div class="feature_card">
+  let wrapper = '<div class="wrapper">';
+  for (let i = 0; i < count; i += 1) {
+    const speaker = `<div class="feature_card">
             <div class="icon">
                 <img src=${data[i].img_path} alt="">
             </div>
@@ -87,27 +65,61 @@ function populateFutureSpeakers(count) {
                 <p class="description">${data[i].description}</p>
             </div>
         </div>`;
-        wrapper = wrapper + speaker;
-    }
-    
-    wrapper = wrapper + '</div>';
-    parent.innerHTML = parent.innerHTML  + wrapper;
+    wrapper += speaker;
+  }
 
-    if (count === 2) {
-        const btn = `<div class="btn_more" id="btn_more">
-                <a href="#">MORE <img src="static/images/arrow_down 1.png" alt=""></a>
-            </div>`
-            parent.innerHTML = parent.innerHTML + btn;
-    }
+  wrapper += '</div>';
+  parent.innerHTML += wrapper;
+
+  if (count === 2) {
+    const btn = `<div class="btn_more" id="btn_more">
+    <a href="#">MORE <img src="static/images/arrow_up.png" alt=""></a>
+    </div>`;
+    parent.innerHTML += btn;
+  } else {
+    const btn = `<div class="btn_more" id="btn_more">
+    <a href="#">LESS <img src="static/images/arrow_down 1.png" alt=""></a>
+    </div>`;
+    parent.innerHTML += btn;
+  }
 }
-populateFutureSpeakers(2);
 
 function moreButtonListener() {
-    const more = document.getElementById('btn_more');
-    more.addEventListener('click', (event) => {
-        event.preventDefault();
-        count = data.length;
+  const more = document.getElementById('btn_more');
+  if (more) {
+    more.addEventListener('click', (event1) => {
+      event1.preventDefault();
+      if (flagExpanded) {
+        const count = 2;
+        flagExpanded = false;
         populateFutureSpeakers(count);
+      } else {
+        const count = data.length;
+        flagExpanded = true;
+        populateFutureSpeakers(count);
+      }
     });
+  }
 }
-moreButtonListener();
+
+window.addEventListener('resize', () => {
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    const count = data.length;
+    populateFutureSpeakers(count);
+  } else {
+    const count = 2;
+    populateFutureSpeakers(count);
+  }
+  moreButtonListener();
+});
+
+window.addEventListener('load', () => {
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    const count = data.length;
+    populateFutureSpeakers(count);
+  } else {
+    const count = 2;
+    populateFutureSpeakers(count);
+  }
+  moreButtonListener();
+});
